b2efbfe2518358deabaae9066ccdb60eb63c8bab
cameron <hammerbro_97@hotmail.com>
2023-03-29 16:55:13 -0600

    added template

---
+++ js/app.js
@@ -0,0 +1,21 @@
+function change_the_image(image){
+
+if(image[`target`].getAttribute(`src`) === `/images/399.png`){
+
+    image[`target`].setAttribute(`src`, `https://www.serebii.net/swordshield/pokemon/399.png`);
+
+}else{
+
+    image[`target`].setAttribute(`src`, `/images/399.png`);
+}
+
+};
+
+let change_image = document.querySelectorAll(`.page_span`);
+
+for(counter = 0; counter < change_image.length; counter = counter +1){
+
+change_image[counter].addEventListener(`click`, change_the_image)
+
+
+}
