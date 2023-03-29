69d0bd2f5bc8c3137d15474c51527b70f96a1ac7
cameron <hammerbro_97@hotmail.com>
2023-03-29 17:33:10 -0600

    attempt 1 in progress

--- js/app.js
+++ js/app.js
@@ -20,3 +20,51 @@ change_image[counter].addEventListener(`click`, change_the_image)
 
 };
 
+function poke_select(details){
+
+    Cookies.set(`selection`, details[`target`].getAttribute(`pokemon`));
+};
+
+let choice = document.querySelectorAll(`.button`);
+for(let  counter = 0; counter < choice.length; counter = counter +1){
+
+    choice[counter].addEventListener(`click`, poke_select);
+};
+
+let bidoof ={
+
+name: `Bidoof`,
+
+description: `the cutest ever`,
+
+image_url: `https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimg.pokemondb.net%2Fartwork%2Flarge%2Fbidoof.jpg&f=1&nofb=1&ipt=0b86aec329ae6117399a94c423068d01bbf47c7083141410007ce7933c8e02fc&ipo=images`
+
+};
+
+let bibarel ={
+
+
+    name: `Bibarel`,
+
+    description: `the worst evolution ever`,
+
+    image_url: `gross`
+
+
+};
+
+let purugly = {
+
+
+    name: `purugly`,
+
+    description: `fat cat`,
+
+    image_url: `fat`
+};
+
+let chosen_pokemon = Cookies.get(`selection`)
+
+
+
+
